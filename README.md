8093bfb52557159cb49c8b01eb8123941336d6af
pmonk1487 <pmonk1487@gmail.com>
2018-06-19 23:41:51 -0700

    RetroPie Cheatfile Script
    A python script to create Retropie .cht files.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Retropie Cheatfile Script
+Quick and dirty script to create .cht files.  <br>
+I ran into small frustrations when adding cheats to files and making my own. <br>
+This won't add cheats to an existing file, but it made it quicker for me to create new files. <br>
+If this helps anyone, I'm glad. <br>
+Also, make sure you add .cht to the filename when it ask you for it.<br>
+It's not vital, but it saves you from adding it later.<br>
+<br>
+DISCLAIMER: It shouldn't do any harm to any of your devices, but use at your own risk.  <br>
+It doesn't check if it is overwriting files. <br>
+You're on your own to make sure you don't write over anything important.<br>
+I keep it in a separate folder since I have it create files within its folder.<br>
---
+++ cheat make.py
@@ -0,0 +1,43 @@
+total=0
+more="Yes"
+cheats=[]
+
+#Gather information
+while more=="Yes":
+    cheatName=str(raw_input("What is the name of the cheat?"))
+
+    autoEnable=str(raw_input("Enable the cheat: True/False"))
+    autoEnable=autoEnable.lower() #Validate input
+    while autoEnable != 'true' and autoEnable !='false':
+        print("Enable the cheat: True/False")
+        autoEnable=str(raw_input("MUST be \'True\' or \'False\'"))
+        autoEnable=autoEnable.lower()
+
+    numLines=input("How many lines are in the cheat?")
+
+    for i in range(numLines): #Input cheats line-by-line
+        line=str(raw_input("What is line {0}?".format(i+1)))
+        if numLines !=1:
+            chtName=cheatName+str(" {0}/{1}".format(i+1,numLines))
+        else:
+            chtName=cheatName
+        cheats.append("cheat{0}_desc = \"{1}\"\ncheat{0}_code = \"{2}\"\ncheat{0}_enable = {3}".format(i+total,chtName,line,autoEnable))
+    total+=numLines #Update total lines
+    more=raw_input("More? Yes/No").title()
+    while more != "Yes" and more != "No": #Validate input
+        print("More? Yes/No")
+        more=raw_input("MUST be \'Yes\' or \'No\'").title()
+
+#Write to file
+filename=str(raw_input("Save To...?"))
+
+file = open(filename,'w') 
+ 
+file.write("cheats = {}".format(total))
+
+for x in cheats:
+    file.write("\n\n")
+    file.write(x)
+
+file.close()
+    
